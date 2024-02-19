4f2e73b794509fa85a714f0de09fcef6ced642f8
aehamdy <ae.hamdy91@gmail.com>
2024-02-19 16:56:44 +0200

    Added show error message function when user inserts an invalid text

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let inputField = document.querySelector(".app__input");
 const addButton = document.querySelector(".app__submit");
+let errorElement = document.querySelector(".app__error");
 const tasksList = document.querySelector(".app__tasks");
 
+function showError(message) {
+    errorElement.textContent = message;
+    errorElement.style.display = "block";
+    inputField.value = "";
+}
+
 const addTask = function () {
-    let userInput = inputField.value;
+    let userInput = inputField.value.trim();
+
+    if (!userInput) {
+
+        showError("Please enter a valid task");
+        return;
+
+    } else {
+        errorElement.innerText = "";
 
     const task = `<li class="app__task" draggable="true">
     <input class="app__checkbox" type="checkbox">
@@ -12,21 +27,22 @@ const addTask = function () {
     <span class="app__delete">X</span>
     </li>`;
 
-    
+    console.log(task);
 
     inputField.value = "";
-}
+    };
+};
 
 addButton.addEventListener("click", addTask);
 
 
 /** #TODO
- * [x] add task to the list
- * [ ] show error message when user doesn't insert a text
- * [ ] remove task function
- * [ ] update database function
- * [ ] clear completed task function
- * [ ] show only active tasks function
- * [ ] show only completed tasks function
- * [ ] a variable to count the unchecked tasks
+ * #[x] add task to the list
+ * #[x] show error message when user inserts invalid text
+ * #[ ] update database function
+ * #[ ] remove task function
+ * #[ ] clear completed task function
+ * #[ ] show only active tasks function
+ * #[ ] show only completed tasks function
+ * #[ ] a variable to count the unchecked tasks
  */
