2e2d1d37198d165819fab00770fc7de06d669103
aehamdy <ae.hamdy91@gmail.com>
2024-02-19 15:57:40 +0200

    Used seperation of concerns

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let inputField = document.querySelector(".app__input");
 const addButton = document.querySelector(".app__submit");
 const tasksList = document.querySelector(".app__tasks");
 
-    
-addButton.addEventListener("click", () => {
-
+const addTask = function () {
     let userInput = inputField.value;
 
     const task = `<li class="app__task" draggable="true">
@@ -14,5 +12,21 @@ addButton.addEventListener("click", () => {
     <span class="app__delete">X</span>
     </li>`;
 
+    
+
     inputField.value = "";
-})
+}
+
+addButton.addEventListener("click", addTask);
+
+
+/** #TODO
+ * [x] add task to the list
+ * [ ] show error message when user doesn't insert a text
+ * [ ] remove task function
+ * [ ] update database function
+ * [ ] clear completed task function
+ * [ ] show only active tasks function
+ * [ ] show only completed tasks function
+ * [ ] a variable to count the unchecked tasks
+ */
